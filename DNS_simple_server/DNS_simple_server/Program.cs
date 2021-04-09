afed2346718734414ad7a42a3266d1e6304a31e7
Rytis Cepulis <cepRytis@gmail.com>
2021-04-09 17:23:01 +0300

    created project and gitignore

---
+++ DNS_simple_server/DNS_simple_server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNS_simple_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
