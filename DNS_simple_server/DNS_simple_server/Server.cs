245ccdd1745c2cf15ed47ddd02bdeb6430c5965e
Rytis Cepulis <cepRytis@gmail.com>
2021-04-10 00:40:25 +0300

    dns server is port 53 and UDP

--- DNS_simple_server/DNS_simple_server/Server.cs
+++ DNS_simple_server/DNS_simple_server/Server.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 namespace DNS_simple_server
 {
     public class Server
     {
-        private readonly int port = 20000;
+        private readonly int port = 53;
         private readonly IPAddress ip = IPAddress.Parse("127.0.0.1");
         private readonly int maxLen = 253;
 
@@ -18,7 +17,7 @@ namespace DNS_simple_server
 
             try
             {
-                socFd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socFd = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
                 socFd.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
                 socFd.Bind(new IPEndPoint(ip, port));
             }
@@ -31,23 +30,10 @@ namespace DNS_simple_server
             {
                 try
                 {
-                    socFd.Listen(1);
-                    Console.WriteLine("Listening...");
-                    var newSoc = socFd.Accept();
-                    var recBytes = 1;
-
-                    while (recBytes != 0)
-                    {
-                        byte[] buffer = new byte[maxLen];
-
-                        recBytes = newSoc.Receive(buffer, 0, maxLen, 0);
-
-                        if (recBytes > 0)
-                        {
-                            Console.WriteLine("Received " + Encoding.UTF8.GetString(buffer));
-                        }
-                    }
-                    newSoc.Close();
+                    int recBytes;
+                    byte[] buffer = new byte[maxLen];
+                    recBytes = socFd.Receive(buffer, 0, maxLen, 0);
+                    Console.WriteLine("Receiving...");
                 }
                 catch (Exception e)
                 {
