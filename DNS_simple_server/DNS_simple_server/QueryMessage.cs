7ad33280b0a470d942f081878e3358dda4f3b2fe
Rytis Cepulis <cepRytis@gmail.com>
2021-04-13 15:15:08 +0300

    parse query other fields than question

--- DNS_simple_server/DNS_simple_server/QueryMessage.cs
+++ DNS_simple_server/DNS_simple_server/QueryMessage.cs
@@ -7,9 +7,34 @@ namespace DNS_simple_server
 {
     public class QueryMessage
     {
-        private readonly int maxLen = 513;
+        //    Question
+        //        255 of less - domainName
+        //        2 QTYPE
+        //        2 QCLASS
+
+        private const int maxLen = 513;
+        private const int messageIdLen = 2;
+        private const int recDesBitLen = 2;
+        private const int nmQuestionsLen = 2;
+        private const int nmAnswersLen = 2;
+        private const int nameServerRecLen = 2;
+        private const int addServerRecLen = 2;
+        private const int qTYPELen = 2;
+        private const int qCLASSLen = 2;
+
         public byte[] Question { get; }
+        public byte[] MessageId { get; } = new byte[messageIdLen];
+        public byte[] RecDesBit { get; } = new byte[recDesBitLen];
+        public byte[] NmQuestions { get; } = new byte[nmQuestionsLen];
+        public byte[] NmAnswers { get; } = new byte[nmAnswersLen];
+        public byte[] NameServerRec { get; } = new byte[nameServerRecLen];
+        public byte[] AddServerRec { get; } = new byte[addServerRecLen];
+        public byte[] QTYPE { get; } = new byte[qTYPELen];
+        public byte[] QCLASS { get; } = new byte[qCLASSLen];
+
+
         public string ParsedDomainName { get; }
+
 
 
         public QueryMessage(Socket socFd)
@@ -26,16 +51,34 @@ namespace DNS_simple_server
                 Console.WriteLine("Exception occured: \n\n" + e.ToString() + "\n");
             }
 
-            //fix temp offset
-            var offset = 12;
+            int offset = 0;
+
+            MessageId = ParseBlock(buffer, messageIdLen, ref offset);
+            RecDesBit = ParseBlock(buffer, recDesBitLen, ref offset);
+            NmQuestions = ParseBlock(buffer, nmQuestionsLen, ref offset);
+            NmAnswers = ParseBlock(buffer, nmAnswersLen, ref offset);
+            NameServerRec = ParseBlock(buffer, nameServerRecLen, ref offset);
+            AddServerRec = ParseBlock(buffer, addServerRecLen, ref offset);
+
             var initOffset = offset;
             ParsedDomainName = ParseDomainName(buffer, ref offset);
             Question = ParseQuestion(offset, initOffset, buffer);
+
+            QTYPE = ParseBlock(buffer, qTYPELen, ref offset);
+            QCLASS = ParseBlock(buffer, qCLASSLen, ref offset);
 
             //fix delete
             Console.WriteLine("Received: " + System.Text.Encoding.Default.GetString(buffer));
             Console.WriteLine("Response domainName: " + System.Text.Encoding.Default.GetString(Question));
             Console.WriteLine("Parsed: " + ParsedDomainName + "\n");
+        }
+
+        byte[] ParseBlock(byte[] buffer, int blockLen, ref int offset)
+        {
+            byte[] parsedBlock = new byte[blockLen];
+            Array.Copy(buffer, offset, parsedBlock, 0, blockLen);
+            offset += blockLen;
+            return parsedBlock;
         }
 
         byte[] ParseQuestion(int offset, int initOffset, byte[] buffer)
