47e8d54a2869f73da2cf63081a8cc72c0ffb3ed5
Rytis Cepulis <cepRytis@gmail.com>
2021-04-13 14:00:25 +0300

    methods for parsing domain name

--- DNS_simple_server/DNS_simple_server/Server.cs
+++ DNS_simple_server/DNS_simple_server/Server.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.RegularExpressions;
 
 namespace DNS_simple_server
 {
+
+
     public class Server
     {
         private readonly int port = 53;
         private readonly IPAddress dnsIp = IPAddress.Parse("127.0.0.1");
-        private readonly int maxLen = 253;
+        private readonly int maxLen = 513;
 
         private readonly string dnsTablePath = @"../../../../dnsTable.txt";
         private readonly Dictionary<string, string> dnsTable = new Dictionary<string, string>();
@@ -34,18 +37,54 @@ namespace DNS_simple_server
 
             while (true)
             {
+                byte[] buffer = new byte[maxLen];
+
                 try
                 {
                     int recBytes;
-                    byte[] buffer = new byte[maxLen];
                     recBytes = socFd.Receive(buffer, 0, maxLen, 0);
-                    Console.WriteLine("Receiving...");
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Exception occured: \n\n" + e.ToString() + "\n");
                 }
+
+                var offset = 12;
+                var domainName = ParseDomainName(buffer, ref offset);
+
+
+                //fix delete
+                Console.WriteLine("Received: " + System.Text.Encoding.Default.GetString(buffer));
+                Console.WriteLine("Parsed: " + domainName + "\n");
             }
+        }
+
+        string ParseDomainName(byte[] buffer, ref int offset)
+        {
+            var labels = new List<string>();
+
+            for (int i = 0; Convert.ToInt16(buffer[offset]) != 0; i++)
+            {
+                labels.Add(ParseLabel(buffer, ref offset));
+            }
+
+            return labels.Aggregate((i, j) => i + "." + j);
+        }
+
+        string ParseLabel(byte[] buffer, ref int offset)
+        {
+            var label = new byte[maxLen];
+            var labelLen = Convert.ToInt16(buffer[offset]);
+            int i;
+
+            for (i = 1; i <= labelLen; i++)
+            {
+                label[i] = buffer[offset + i];
+                System.Text.Encoding.Default.GetString(buffer);
+            }
+
+            offset += i;
+            return System.Text.Encoding.Default.GetString(label).TrimEnd('\0').TrimStart('\0');
         }
 
         private void GetDnsTable()
